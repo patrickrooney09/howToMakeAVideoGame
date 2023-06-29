efa50d3fcc927a377c52953f390bf1ea0dafbf3f
Patrick Rooney <patrickrooney09@gmail.com>
2023-06-29 15:25:12 -0400

    added scripts for game over state and for restarting scene

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if(collisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
         
     }

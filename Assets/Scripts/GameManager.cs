4d6a719f056dc030915f8b02c9c0b8ee71a6f8d4
Patrick Rooney <patrickrooney09@gmail.com>
2023-07-10 17:35:11 -0400

    level complete animation working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,19 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
 
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame()
     {
         if(gameHasEnded == false)
         {
             gameHasEnded = true;
+            Debug.Log("GAME OVER");
             Invoke("Restart", restartDelay);
         }
         
